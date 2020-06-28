925da16d7baca2a48f490b92f0267ab1ba98d264
gozdekosa <gzd_508@hotmail.com>
2020-06-28 15:52:32 +0300

    init

---
+++ Assets/buttonhandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonhandler : MonoBehaviour {
+
+	public void RestartGame()
+    {
+        SceneManager.LoadScene("E_ARCore_Base");
+    }
+}
